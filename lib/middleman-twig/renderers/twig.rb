7828199383513ce041cfcd454740c16529082dcf
Diogo Tome <khdiogo@hotmail.com>
2015-12-23 17:21:26 +0000

    Fixed bug when using middleman-twig without page_io

---
+++ lib/middleman-twig/renderers/twig.rb
@@ -0,0 +1,34 @@
+require 'twig'
+require 'middleman-core/templates'
+
+# Twig Renderer
+module Middleman
+  module Renderers
+    module Twig
+      class << self
+        def registered(app)
+          app.before_configuration do
+            template_extensions twig: :html
+          end
+          # Twig is not included in the default gems,
+          # but we'll support it if available.
+
+          # After config, setup mustache partial paths
+          app.after_configuration do
+            ::Twig::Template.file_system = ::Twig::LocalFileSystem.new(source_dir)
+
+            # Convert data object into a hash for twig
+            sitemap.provides_metadata %r{\.twig$} do
+              locals_hash = { :locals => { :data => data.to_h } }
+              locals_hash[:locals].merge!({ :page_io => page_io }) if defined?(page_io) and page_io.present?
+              locals_hash
+            end
+          end
+        end
+
+        alias :included :registered
+      end
+    end
+  end
+end
+#Middleman::Templates.register :twig, Middleman::Renderers::Twig
